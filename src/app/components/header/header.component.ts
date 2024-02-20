5050c4d7d15b87cb813a7accbf3aa538706ef96a
AngelSmithlgs <angelsmithlgs@gmail.com>
2024-02-20 14:04:51 -0300

    fix the cart, now is working

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: `
-    <p>
-      header works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl:'./header.component.html',
+
+ 
 })
 export class HeaderComponent implements OnInit {
 
